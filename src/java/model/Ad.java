ba2c078c257f58202c4aae8e0214571a0ad208f5
LAPTOP-1LT11E6G\user <maksoev@gmail.com>
2018-01-12 01:29:33 +0600

    Fix Ad model

--- src/java/model/Ad.java
+++ src/java/model/Ad.java
@@ -23,8 +23,26 @@ public class Ad {
     private byte[] image;
     private int user_id;
     private int category_id;
+    private User user;
+    private Category category;
+    
     
     public Ad() {
+        
+    }
+
+    public Ad(int id, String name, String description, int price, String city, Date datepost, byte[] image, int user_id, int category_id, User user, Category category) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.city = city;
+        this.datepost = datepost;
+        this.image = image;
+        this.user_id = user_id;
+        this.category_id = category_id;
+        this.user = user;
+        this.category = category;
     }
 
     public Ad(int id, String name, String description, int price, String city, Date datepost, byte[] image, int user_id, int category_id) {
@@ -37,7 +55,17 @@ public class Ad {
         this.image = image;
         this.user_id = user_id;
         this.category_id = category_id;
-        
+    }
+
+    public Ad(int id, String name, String description, int price, String city, Date datepost, int user_id, int category_id) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.city = city;
+        this.datepost = datepost;
+        this.user_id = user_id;
+        this.category_id = category_id;
     }
 
     public int getId() {
@@ -76,6 +104,14 @@ public class Ad {
         return category_id;
     }
 
+    public User getUser() {
+        return user;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
     public void setId(int id) {
         this.id = id;
     }
@@ -112,9 +148,74 @@ public class Ad {
         this.category_id = category_id;
     }
 
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 23 * hash + this.id;
+        hash = 23 * hash + Objects.hashCode(this.name);
+        hash = 23 * hash + Objects.hashCode(this.description);
+        hash = 23 * hash + this.price;
+        hash = 23 * hash + Objects.hashCode(this.city);
+        hash = 23 * hash + Objects.hashCode(this.datepost);
+        hash = 23 * hash + this.user_id;
+        hash = 23 * hash + this.category_id;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Ad other = (Ad) obj;
+        if (this.id != other.id) {
+            return false;
+        }
+        if (this.price != other.price) {
+            return false;
+        }
+        if (this.user_id != other.user_id) {
+            return false;
+        }
+        if (this.category_id != other.category_id) {
+            return false;
+        }
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        if (!Objects.equals(this.description, other.description)) {
+            return false;
+        }
+        if (!Objects.equals(this.city, other.city)) {
+            return false;
+        }
+        if (!Objects.equals(this.datepost, other.datepost)) {
+            return false;
+        }
+        if (!Arrays.equals(this.image, other.image)) {
+            return false;
+        }
+        return true;
+    }
+
     @Override
     public String toString() {
-        return "Ad{" + "id=" + id + ", name=" + name + ", description=" + description + ", price=" + price + ", city=" + city + ", datepost=" + datepost + ", image=" + image + ", user_id=" + user_id + ", category_id=" + category_id + '}';
+        return "Ad{" + "id=" + id + ", name=" + name + ", description=" + description + ", price=" + price + ", city=" + city + ", datepost=" + datepost + ", image=" + image + ", user_id=" + user_id + ", category_id=" + category_id + ", user=" + user + ", category=" + category + '}';
     }
+
 
 }
